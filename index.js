ab644de7ca62e87570dfb0c9727f6d9acfcdcce0
GilbertHLam <gilberthlam@cmail.carleton.ca>
2018-11-03 17:21:05 -0400

    Quick trial

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const { initDbs } = require('./db');
@@ -10,11 +11,7 @@ const port = process.env.PORT || 4000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.all(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors());
 
 
 // Authentication routes
