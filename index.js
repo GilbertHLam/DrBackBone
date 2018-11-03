0fc136892b1e80a7fb236159a9d7d4bf14facef2
GilbertHLam <gilberthlam@cmail.carleton.ca>
2018-11-03 17:17:29 -0400

    using app.all

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 4000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(function (req, res, next) {
+app.all(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
