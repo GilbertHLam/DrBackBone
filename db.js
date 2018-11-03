20d6c046c6497851bd1d6d48111f120770624f36
GilbertHLam <gilberthlam@cmail.carleton.ca>
2018-11-03 16:59:29 -0400

    Added functions for medications

--- db.js
+++ db.js
@@ -1,13 +1,22 @@
 const sqlite3 = require('sqlite3');
 
-let users_db;
+let users_db, medications_db;
 
 function initDbs(callback) {
     users_db = new sqlite3.Database(process.env.DB_FILE, sqlite3.OPEN_READWRITE, (err) => {
         if (err) {
             console.error(err.message);
+        } else {
+            console.log('Connected to the users database.');
         }
-        console.log('Connected to the users database.');
+    });
+
+    medications_db = new sqlite3.Database(process.env.MEDICATION_DB, sqlite3.OPEN_READWRITE, (err) => {
+        if (err) {
+            console.error(err.message);
+        } else {
+            console.log('Connected to the medications database.');
+        }
     });
     return callback();
 }
@@ -16,7 +25,12 @@ function getUsersDb() {
     return users_db;
 }
 
+function getMedicationsDb() {
+    return medications_db;
+}
+
 module.exports = {
     getUsersDb,
-    initDbs
+    initDbs,
+    getMedicationsDb
 };
--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
-const { initDbs} = require('./db');
+const { initDbs } = require('./db');
 const { login, signup } = require('./authentications')
-
+const { getAllMedications, editMedication, addMedication } = require('./medications')
 const port = process.env.PORT || 4000;
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -26,6 +26,18 @@ app.post('/signup', (request, response) => {
     signup(request, response);
 });
 
+//Medication routes
+app.post('/getAllMedications', (request, response) => {
+    getAllMedications(request, response);
+});
+
+app.post('/addMedication', (request, response) => {
+    addMedication(request, response);
+});
+
+app.post('/editMedication', (request, response) => {
+    editMedication(request, response);
+});
 
 initDbs(function () {
     app.listen(port);
